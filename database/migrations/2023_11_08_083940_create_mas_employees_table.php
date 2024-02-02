dd091fe6060bb783390260545d3e27c0f1ceeaa6
bikram-12 <bikramchuwan6@gmail.com>
2024-02-02 12:45:40 +0600

    36 draft

--- database/migrations/2023_11_08_083940_create_mas_employees_table.php
+++ database/migrations/2023_11_08_083940_create_mas_employees_table.php
@@ -19,9 +19,10 @@ return new class extends Migration
             $table->string("last_name",100)->nullable();
             $table->string("emp_id",50)->index();
             $table->date("date_of_appointment");
-            $table->uuid("grade_id")->nullable()->change();
-            $table->uuid("grade_step_id")->nullable()->change();
-            $table->uuid("designation_id")->nullable()->change();
+            $table->uuid("grade_id")->nullable();
+            $table->uuid("grade_step_id")->nullable();
+            $table->uuid('designation_id')->nullable();
+            $table->foreign("designation_id")->on("mas_designations")->references("id")->onDelete('set null');
             $table->unsignedBigInteger('department_id')->nullable();
             $table->foreign('department_id')->references('id')->on('departments')->onDelete('set null');
 
@@ -31,9 +32,8 @@ return new class extends Migration
             $table->unsignedBigInteger('region_id')->nullable();
             $table->foreign('region_id')->references('id')->on('regions')->onDelete('set null');
 
-            $table->foreign("designation_id")->on("mas_designations")->references("id")->restrictOnDelete()->cascadeOnUpdate();
-            $table->foreign("grade_id")->on("mas_grades")->references("id")->restrictOnDelete()->cascadeOnUpdate();
-            $table->foreign("grade_step_id")->on("mas_grade_steps")->references("id")->restrictOnDelete()->cascadeOnUpdate();
+            $table->foreign("grade_id")->on("mas_grades")->references("id")->onDelete('set null');
+            $table->foreign("grade_step_id")->on("mas_grade_steps")->references("id")->onDelete('set null');
             $table->string('employee_display', 350)->storedAs("concat(first_name,case when middle_name is not null then concat(' ',middle_name) else '' end, case when last_name is not null then concat(' ',last_name) else '' end, ' (E00',emp_id,')')");
             $table->string('gender')->nullable();
             $table->string('employment_type')->nullable();
